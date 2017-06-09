b5c5a94a57515bd2d4c5b42f47d0dc9c63538869
srikanthmaturu <srikanthmaturu@outlook.com>
2017-06-09 11:26:18 -0500

    fixes

--- include/sim_hash/sim_hash.hpp
+++ include/sim_hash/sim_hash.hpp
@@ -19,13 +19,13 @@ std::vector<uint64_t> get_hashed_tokens(std::string &string, size_t window_size)
   std::copy(std::istream_iterator<std::string>(iss),
        std::istream_iterator<std::string>(),
        std::back_inserter(tokens));
-  
+
   std::vector<uint64_t> hashes;
   hashes.reserve(tokens.size());
   for(size_t i = 0; i < tokens.size(); ++i) {
     
     std::string supertoken(tokens[i]);
-    for(size_t j = i+1; j < std::min(i+window_size, tokens.size()); ++j) 
+    for(size_t j = i+1; j < std::min(i+window_size, tokens.size()); ++j)
       supertoken.append(" " + tokens[j]);
   
     uint32_t h1, h2;
@@ -44,12 +44,49 @@ std::vector<uint64_t> get_hashed_tokens(std::string &string, size_t window_size)
   return hashes;
 }
 
+std::vector<uint64_t> get_hashed_tokens_v2(std::string &string, size_t window_size) {
+
+    uint32_t seed1 = 0x2c759c01;
+    uint32_t seed2 = 0xfef136d7;
+
+    std::istringstream iss(string);
+    std::vector<std::string> tokens;
+
+    for(uint64_t i=0; i<(string.size()-window_size)+1; i++){
+        tokens.push_back(string.substr(i, window_size));
+        std::cout << "S " << i << " "<< string.substr(i, window_size) << std::endl;
+    }
+    std::vector<uint64_t> hashes;
+    hashes.reserve(tokens.size());
+    for(size_t i = 0; i < tokens.size(); ++i) {
+
+        std::string supertoken(tokens[i]);
+        for(size_t j = i+1; j < std::min(i+window_size, tokens.size()); ++j)
+            supertoken.append(" " + tokens[j]);
+
+        uint32_t h1, h2;
+        MurmurHash3_x86_32(supertoken.c_str(),
+                           supertoken.length(),
+                           seed1,
+                           &h1);
+        MurmurHash3_x86_32(supertoken.c_str(),
+                           supertoken.length(),
+                           seed2,
+                           &h2);
+        uint64_t h = ((uint64_t)h1 << 32) + h2;
+        //std::cout << supertoken << " h:" << h << std::endl;
+        hashes.push_back(h);
+    }
+    return hashes;
+}
+
+
 /* Calculates the similarity hash with super-tokens of length window_size. */
 uint64_t simhash64(std::string &string, size_t window_size) {
   
   std::vector<int> histogram(64, 0);
 
-	std::vector<uint64_t> hashes = get_hashed_tokens(string, window_size);
+	std::vector<uint64_t> hashes = get_hashed_tokens_v2(string, window_size);
 
   for(auto &hash : hashes) {
 		int weight = 1; // token weight is 1 by default; it may change depending on the hashes frequencies
