927f3460d9b39476ec6b4b3efd74877a36f35726
srikanthmaturu <srikanthmaturu@outlook.com>
2017-06-15 15:10:03 -0500

    Fixes

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#make mi_bv_red_index_6
+make mi_bv_red_index_6
 #make mi_bv_red_index_8
-make  sh_mi_bv_red_index_10_ws4
-#make kmers_convertor
-#make fastaparser
+#make  sh_mi_bv_red_index_10_ws4
+make kmers_convertor
+make fastaparser
 #make results_converter
 
